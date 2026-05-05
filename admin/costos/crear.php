6d26f7d7257fe914e3832526492ad3cebfea3824
gabyrubgue <gabrielarubioguevara@gmail.com>
2026-05-04 23:01:49 -0500

    cambio de "precio" a "valor"

--- admin/costos/crear.php
+++ admin/costos/crear.php
@@ -16,7 +16,7 @@ require '../../config/db.php';
         <option value="compartido">Global (entre varios)</option>
     </select>
 
-    <label>Precio </label>
+    <label>Valor </label>
     <input type="number" name="precio" min="0" required>
 
 
